8358a34e388393d61b7de0e5242dbf753dca9c33
username <zeus4797@naver.com>
2023-08-03 01:39:06 +0900

    build

--- pages/api_get.js
+++ pages/api_get.js
@@ -150,18 +150,3 @@ const getDetailData = async (url) => {
     return data;
   }
 };
-
-// const getData = async (url) => {
-//   const res = await axios.get(url, {
-//     headers: { accept: "application/json" },
-//   });
-//   console.log(res);
-//   return res; // axios uses .data to access the response body
-// };
-
-//github에서 배포,엔도상과의 미팅을 통해서 배포할 수 있게 할것
-//계속해서 메시지 송신이 가능하도록 하게 할것, 확인됨
-//jGrants Detail API를 통해서 url과 여러 디테일한 내용을 받아오고, 그것을 보기좋게 표시하고, chatGPT에게도 알려주어서, 제대로 추천을 할 수 있게 한다
-//자기가 송신한 메시지도 리스트에 추가하여 보여줄것
-
-//cors관련 에러 수정할것
